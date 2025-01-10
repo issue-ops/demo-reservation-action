7f72b409959b0038b629be145002717edde4cce4
Nick Alteen <ncalteen@github.com>
2025-01-10 13:40:33 -0500

    Add checks

---
+++ dist/enums.d.ts
@@ -0,0 +1,11 @@
+/** Allowed Activities for the Action */
+export declare enum AllowedActions {
+    /** Initialize the Request */
+    INIT = "init",
+    /** Submit the Request */
+    SUBMIT = "submit",
+    /** Cancel the Request */
+    CANCEL = "cancel",
+    /** Expire the Request */
+    EXPIRE = "expire"
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
+import { AllowedActions } from './enums.js'
 
 /**
  * The entrypoint for the action
  */
 export async function run(): Promise<void> {
+  // Fail if the action is being run on an unsupported event type.
+  if (
+    !['issues', 'issue_comment', 'schedule'].includes(github.context.eventName)
+  )
+    throw new Error(
+      'This action can only be run on issues, issue_comments, and schedule events.'
+    )
+
   // Get the inputs
   const action: string = core.getInput('action', { required: true })
   const issueBody: object = JSON.parse(
@@ -21,4 +31,8 @@ export async function run(): Promise<void> {
   core.info(`  owner: ${owner}`)
   core.info(`  repository: ${repository}`)
   core.info(`  workspace: ${workspace}`)
+
+  // Fail if the specified action isn't valid.
+  if (!Object.values(AllowedActions).includes(action as AllowedActions))
+    throw new Error(`Invalid action: ${action}`)
 }
